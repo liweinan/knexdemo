3d733294e6ca92af690fbdef02090d31fe9e4c48
weli <l.weinan@gmail.com>
2019-05-15 04:30:58 +0800

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var knex = require('knex')({
+let knex = require('knex')({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -8,4 +8,6 @@ var knex = require('knex')({
   }
 });
 
-console.log(knex);
+// console.log(knex);
+
+console.log(knex('flyway_schema_history').toSQL().toNative());
